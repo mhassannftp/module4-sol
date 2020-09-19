9bb20a0a663c8e6d0e4762c9e94673fdb63c656c
Muhammad Hassan <69034480+mhassannftp@users.noreply.github.com>
2020-09-20 01:45:07 +0500

    Add files via upload

---
+++ module4-sol/js/script.js
@@ -0,0 +1,13 @@
+
+
+
+var names = ["Abid" , "Baber", "Cliny", "David", "Farhan", "Gold", "Henry", "Inna"];
+
+
+for (var i=0; i < names.length; i++) {
+	console.log("Hello " + names[i]);
+}
+names[8] = "Jhon";
+	// for (var i = 0; i < names.length; i++) {
+		console.log("Goodbye " + names[i]);
+	// }
